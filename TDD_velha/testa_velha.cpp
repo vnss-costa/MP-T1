90d9e12fd343e9df24cd2148d7fc98c9e1b06cbc
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-09-12 21:22:43 -0300

    feat(testa_velha):
    cria testes para vitorias em colunas

--- TDD_velha/testa_velha.cpp
+++ TDD_velha/testa_velha.cpp
@@ -8,10 +8,35 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-TEST_CASE("Testa velha", "[single-file]")
+TEST_CASE("Testa coluna", "[single-file]")
 {
 	int teste1[3][3] = {{2, 0, 1},
 						{2, 0, 1},
 						{0, 2, 1}};
 	REQUIRE(VerificaVelha(teste1) == 1);
+
+	int teste2[3][3] = {{2, 1, 0},
+						{2, 1, 0},
+						{0, 1, 2}};
+	REQUIRE(VerificaVelha(teste2) == 1);
+
+	int teste3[3][3] = {{1, 2, 0},
+						{1, 2, 0},
+						{1, 0, 2}};
+	REQUIRE(VerificaVelha(teste3) == 1);
+
+	int teste4[3][3] = {{2, 1, 0},
+						{2, 1, 0},
+						{2, 0, 1}};
+	REQUIRE(VerificaVelha(teste4) == 2);
+
+	int teste5[3][3] = {{1, 2, 0},
+						{1, 2, 0},
+						{0, 2, 1}};
+	REQUIRE(VerificaVelha(teste5) == 2);
+
+	int teste6[3][3] = {{1, 0, 2},
+						{1, 0, 2},
+						{0, 1, 2}};
+	REQUIRE(VerificaVelha(teste6) == 2);
 }
