580593cfc45d839c581c45b7443d90b97e99fb76
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-09-13 22:47:04 -0300

    feat(testa_velha):
    cria testes para casos indefinidos

--- TDD_velha/testa_velha.cpp
+++ TDD_velha/testa_velha.cpp
@@ -142,3 +142,36 @@ TEST_CASE("Testa indefinidos", "[single-file]")
                         {0, 0, 0}};
     REQUIRE(VerificaVelha(teste4) == -1);
 }
+
+TEST_CASE("Testa impossiveis", "[single-file]")
+{
+    int teste1[3][3] = {{2, 2, 2},
+                        {2, 1, 1},
+                        {1, 1, 1}};
+    REQUIRE(VerificaVelha(teste1) == -2);
+
+    int teste1[3][3] = {{0, 0, 0},
+                        {0, 1, 1},
+                        {1, 0, 1}};
+    REQUIRE(VerificaVelha(teste1) == -2);
+
+    int teste1[3][3] = {{2, 2, 2},
+                        {2, 0, 0},
+                        {0, 0, 0}};
+    REQUIRE(VerificaVelha(teste1) == -2);
+
+    int teste2[3][3] = {{2, 2, 1},
+                        {2, 2, 1},
+                        {1, 0, 2}};
+    REQUIRE(VerificaVelha(teste2) == -2);
+
+    int teste3[3][3] = {{1, 0, 0},
+                        {0, 1, 1},
+                        {1, 2, 2}};
+    REQUIRE(VerificaVelha(teste3) == -2);
+
+    int teste4[3][3] = {{10, 0, 0},
+                        {0, 19, 0},
+                        {0, 0, 0}};
+    REQUIRE(VerificaVelha(teste4) == -2);
+}
