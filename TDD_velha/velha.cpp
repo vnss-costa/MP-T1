411bef860eb16793e3d58cdfd24d82d021745e68
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-09-14 00:02:14 -0300

    feat(velha):
    cria codigo para testar casos impossiveis

--- TDD_velha/velha.cpp
+++ TDD_velha/velha.cpp
@@ -12,7 +12,97 @@
 
 int VerificaVelha(int velha[3][3])
 {
-    int jogador1 = 0, jogador2 = 0, empates = 0;
+    int jogador1 = 0, jogador2 = 0, ganhadores = 0, quantidade1 = 0, quantidade2 = 0;
+
+    //verifica se a quantidade de 1s é maior que a de 2s
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (velha[i][j] == 1)
+                quantidade1++;
+            else if (velha[i][j] == 2)
+                quantidade2++;
+        }
+    }
+    if (((quantidade2 - quantidade1) > 1) || (quantidade1 - quantidade2 > 1))
+        return -2; //se algum número diferente de 0, 1 ou 2 for encontrado
+
+    //verifica se houveram 2 ganhadores nas colunas
+    for (int j = 0; j < 3; j++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (velha[i][j] == 1)
+                jogador1++;
+            else if (velha[i][j] == 2)
+                jogador2++;
+        }
+        if (jogador1 == 3)
+        {
+            ganhadores++;
+            jogador1++;
+        }
+        else if (jogador1 < 3)
+            jogador1 = 0;
+
+        if (jogador2 == 3)
+        {
+            ganhadores++;
+            jogador2++;
+        }
+        else if (jogador2 < 3)
+            jogador2 = 0;
+    }
+    if (ganhadores > 1)
+        return -2;
+
+    jogador1 = 0;
+    jogador2 = 0;
+    ganhadores = 0;
+
+    //verifica se houveram 2 ganhadores nas linhas
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (velha[i][j] == 1)
+                jogador1++;
+            else if (velha[i][j] == 2)
+                jogador2++;
+        }
+        if (jogador1 == 3)
+        {
+            ganhadores++;
+            jogador1++;
+        }
+        else if (jogador1 < 3)
+            jogador1 = 0;
+
+        if (jogador2 == 3)
+        {
+            ganhadores++;
+            jogador2++;
+        }
+        else if (jogador2 < 3)
+            jogador2 = 0;
+    }
+    if (ganhadores > 1)
+        return -2;
+
+    jogador1 = 0;
+    jogador2 = 0;
+
+    //verifica se foi inserido algo diferente de 1s ou 2s
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if ((velha[i][j] != 0) && (velha[i][j] != 1) && (velha[i][j] != 2))
+                return -2; //se algum número diferente de 0, 1 ou 2 for encontrado
+        }
+    }
+
     //verifica ganhador em colunas
     for (int j = 0; j < 3; j++)
     {
@@ -85,15 +175,14 @@ int VerificaVelha(int velha[3][3])
     jogador1 = 0;
     jogador2 = 0;
 
-    //verifica indefinidos
+    //verifica indefinidos e empates
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
             if (velha[i][j] == 0)
-                empates++; //guarda se algum 0 foi encontrado
+                return -1; //se algum 0 for encontrado, retorna indefinido
         }
     }
-    if (empates == 0)
-        return 0; // se não foi encontrado nenhum 0, retorna empate
+    return 0; // se não foi encontrado nenhum 0, retorna empate
 }
