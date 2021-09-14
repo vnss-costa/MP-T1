d771665f4788ea3b099a27dfcdf5956d0cb4da95
Vanessa Paixão <vanessapaixao@cjr.org.br>
2021-09-13 21:30:50 -0300

    feat(velha):
    verifica se ha ganhadores nas diagonais

--- TDD_velha/velha.cpp
+++ TDD_velha/velha.cpp
@@ -12,42 +12,75 @@
 
 int VerificaVelha(int velha[3][3])
 {
-	int jogador1 = 0, jogador2 = 0;
-	for (int j = 0; j < 3; j++)
-	{
-		for (int i = 0; i < 3; i++)
-		{
-			if (velha[i][j] == 1)
-				jogador1++;
-			else if (velha[i][j] == 2)
-				jogador2++;
-		}
-		if (jogador1 == 3)
-			return 1;
+    int jogador1 = 0, jogador2 = 0;
+    for (int j = 0; j < 3; j++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (velha[i][j] == 1)
+                jogador1++;
+            else if (velha[i][j] == 2)
+                jogador2++;
+        }
+        if (jogador1 == 3)
+            return 1;
 
-		if (jogador2 == 3)
-			return 2;
+        if (jogador2 == 3)
+            return 2;
 
-		jogador1 = 0;
-		jogador2 = 0;
-	}
+        jogador1 = 0;
+        jogador2 = 0;
+    }
 
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (velha[i][j] == 1)
-				jogador1++;
-			else if (velha[i][j] == 2)
-				jogador2++;
-		}
-		if (jogador1 == 3)
-			return 1;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (velha[i][j] == 1)
+                jogador1++;
+            else if (velha[i][j] == 2)
+                jogador2++;
+        }
+        if (jogador1 == 3)
+            return 1;
 
-		if (jogador2 == 3)
-			return 2;
+        if (jogador2 == 3)
+            return 2;
 
-		jogador1 = 0;
-		jogador2 = 0;
-	}
+        jogador1 = 0;
+        jogador2 = 0;
+    }
+    //verifica ganhador na diagonal principal
+    for (int i = 0; i < 3; i++)
+    {
+        if (velha[i][i] == 1)
+            jogador1++;
+        else if (velha[i][i] == 2)
+            jogador2++;
+    }
+    if (jogador1 == 3)
+        return 1;
+
+    if (jogador2 == 3)
+        return 2;
+
+    jogador1 = 0;
+    jogador2 = 0;
+
+    //verifica ganhador na diagonal secundária
+    for (int i = 0; i < 3; i++)
+    {
+        if (velha[i][2 - i] == 1)
+            jogador1++;
+        else if (velha[i][2 - i] == 2)
+            jogador2++;
+    }
+    if (jogador1 == 3)
+        return 1;
+
+    if (jogador2 == 3)
+        return 2;
+
+    jogador1 = 0;
+    jogador2 = 0;
 }
